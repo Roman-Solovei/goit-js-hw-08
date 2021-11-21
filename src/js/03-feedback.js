b016b39771864e8f307276297be315bb3a04a749
Roman-Solovei <romario2409@gmail.com>
2021-11-21 22:33:25 +0200

    033

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,23 +11,45 @@
 // };
 
 // inputName.addEventListener('input', outName);
+
+const inputEmail = document.querySelector('input[type=email]');
+const inputMessage = document.querySelector('textarea[name=message]');
+
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', handleSubmit);
 
-function handleSubmit(event) {
+function handleSubmit() {
   
-  event.preventDefault();
-    
-    const inputEmail = document.querySelector('input[type=email]');
-    const inputMessage = document.querySelector('textarea[name=message]');
+//   event.preventDefault();    
 
-    const inputData = {
+    const formData = {
         email: inputEmail.value,
         message: inputMessage.value,
     };
 
-    localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 
     console.log(`E-mail: ${inputEmail.value}, Message: ${inputMessage.value}`);    
 }
+
+// const submit = document.querySelector('button[type=submit]');
+// console.log(submit)
+
+
+//  function qqq() {
+
+const aaa = localStorage.getItem('feedback-form-state');
+console.log(aaa);    
+     
+const sss = JSON.parse(aaa);
+console.log(sss.email);
+ inputEmail.value = sss.email;
+ inputMessage.value = sss.message;
+
+// };
+
+
+form.addEventListener('submit', qqq);
+
+
