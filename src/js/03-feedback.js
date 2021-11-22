3a9b14bbd8251e742c68d0117481e3019dd3e97f
Roman-Solovei <romario2409@gmail.com>
2021-11-22 23:38:19 +0200

    03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,15 @@
-
-
-
-// inputEmail.addEventListener('input', console.log(inputEmail.value));
-
-// let inputMessage = document.querySelector('.feedback-form textarea[name=message]');
-// console.log(inputMessage.value)
-
-// const outName = () => {     
-//     inputName.value === '' ? outputName.innerHTML = 'Anonymous' : outputName.innerHTML = inputName.value;    
-// };
-
-// inputName.addEventListener('input', outName);
-
 const inputEmail = document.querySelector('input[type=email]');
 const inputMessage = document.querySelector('textarea[name=message]');
-
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', handleSubmit);
+const button = document.querySelector('button[type=submit]');
 
-function handleSubmit() {
-  
-//   event.preventDefault();    
+var throttle = require('lodash.throttle');
+
+form.addEventListener('input', throttle(handleSubmit, 500));
+form.addEventListener('input', formValue());
+
+function handleSubmit() {  
 
     const formData = {
         email: inputEmail.value,
@@ -29,27 +17,21 @@ function handleSubmit() {
     };
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-
-    console.log(`E-mail: ${inputEmail.value}, Message: ${inputMessage.value}`);    
+    console.log(`E-mail: ${inputEmail.value}, Message: ${inputMessage.value}`);
 }
 
-// const submit = document.querySelector('button[type=submit]');
-// console.log(submit)
+function formValue() {
+const feedbackFormState = localStorage.getItem('feedback-form-state');    
+const currentFormState = JSON.parse(feedbackFormState);
+    
+inputEmail.value = currentFormState.email;    
+inputMessage.value = currentFormState.message;    
+};
 
+function formSubmit() {    
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+    console.log('qwe')
+}
 
-//  function qqq() {
-
-const aaa = localStorage.getItem('feedback-form-state');
-console.log(aaa);    
-     
-const sss = JSON.parse(aaa);
-console.log(sss.email);
- inputEmail.value = sss.email;
- inputMessage.value = sss.message;
-
-// };
-
-
-form.addEventListener('submit', qqq);
-
-
+form.addEventListener('submit', formSubmit);
